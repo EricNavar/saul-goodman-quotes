7b0c1d61e92c4fa060dafdbc9189774b5bf6331e
Eric Navar <ericnavar@ufl.edu>
2023-05-02 18:38:24 -0400

    use API gateway to get quotes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # saul-goodman-quotes
 
 - The only node package you will ever need
-- Request up to almost 16 unique quotes by Saul
+- Request up to almost ~~16~~**52** unique quotes by Saul Goodman and Jimmy McGill
 
 ![billboard](https://user-images.githubusercontent.com/32403644/200190190-cbbd4f4a-6d43-40fd-883d-e98582b648fe.png)
 
@@ -11,13 +11,19 @@
 ```
 import { getQuote, getAllQuotes } from '@ericnavar/saul-goodman-quotes'
 
-//returns a random quote (returns string)
-getQuote();
+// returns all quotes 
+// string[]
+getAllQuotes()
 
-//returns all quotes (returns string[])
-getAllQuotes();
+// returns a random quote
+// string
+getQuote()
 
 ```
+
+## API
+- The Lambda code to power this amazing package is here: https://github.com/EricNavar/saul-quotes-serverless
+- The API is also open to contributions
 
 ## Contributing
 
--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,24 +1,35 @@
-const quotes = [
-    'Clearly, his taste in women is the same as his taste in lawyers.',
-    'You two suck at peddling meth.',
-    'Don\'t drink and drive but if you do, call me.',
-    'Congratulations, you’ve just left your family a second-hand Subaru.',
-    'Better call Saul!',
-    'I’m not saying it’s not bad. It’s bad. But it could be worse.',
-    'What the hell is wrong with you? You act like you’re the first guy this ever happened to. I caught my second wife screwing my stepdad, okay? It’s a cruel world, Walt. Grow up.',
-    'What Did You Expect? Haji\'s Quick Vanish?Walt Told Me You Took A Run At This Bogdan Character, And He Wrestled You Into Submission With His Eyebrows.',
-    'Did The Academy Hire You Right Out Of The Womb? You Guys Get Younger And Younger Every Year. We Have Laws, Detective, Have Your Kindergarten Teacher Read Them To You.',
-    'Congratulations! You\'re Now Officially The Cute One Of The Group.',
-    'If You\'re Committed Enough, You Can Make Any Story Work. I Once Convinced A Woman I Was Kevin Costner, And It Worked, Because I Believed It!',
-    'Look, Let’s Start With Some Tough Love, All Right? Ready For This? Here Goes: You Two Suck At Peddling Meth. Period.',
-    'If I Ever Get Anal Polyps, At Least I Know What To Name Them.',
-    'He Said He\'ll Break My Legs, And Don’t Tell Me He Didn’t Mean It... He Gave Me The Dead Mackerel Eyes.',
-    'You pulled that heartstrings con job on me?! You piece of shit! ‘Oh, my brain used to work, I’m sick, I don’t know what to do!’ Asshole! No wonder Rebecca left you! What took her so long?!',
-];
+const axios = require('axios');
 
-export const getQuote = () => {
-    const randomNumber = Math.floor(Math.random() * quotes.length);
-    return quotes[randomNumber];
+const getURI = (random) => {
+    let uri = `https://6qme6dsq0i.execute-api.us-east-1.amazonaws.com/api/quotes`;
+    if (random)
+        uri = uri + '/random';
+    return uri;
+}
+
+const request = async (random) => {
+    const url = getURI(random);
+    return axios.get(url)
+        .then(function (response) {
+            if (response.data && response.data.message)
+                return response.data.message;
+            else
+                return 'fuck';
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 };
 
-export const getAllQuotes = () => quotes;
+const getQuote = () => {
+    return request(true);
+};
+
+const getAllQuotes = () => {
+    return request(false);
+};
+
+module.exports = {getQuote, getAllQuotes};
